8aab1d79462a8ecb151ca1488a46fcf55c52b34b
ericashin221 <36352310+ericashin221@users.noreply.github.com>
2021-06-10 16:35:01 -0600

    remove unnecessary files

--- STARTS/asterisk/workingDir/src/main/java/org/asteriskjava/pbx/CallStateData.java
+++
@@ -1,5 +0,0 @@
-package org.asteriskjava.pbx;
-
-public class CallStateData
-{
-}
--- STARTS/asterisk/workingDir/src/main/java/org/asteriskjava/pbx/asterisk/wrap/events/ChannelEvent.java
+++
@@ -1,8 +0,0 @@
-package org.asteriskjava.pbx.asterisk.wrap.events;
-
-import org.asteriskjava.pbx.Channel;
-
-public interface ChannelEvent
-{
-	Channel getChannel();
-}
